2161ef22f2ea33d7776eaa361f175a9d314c1b6b
jerry <jerry@linux101.net.tw>
2017-12-18 15:27:47 +0800

    Add demo file for test.

--- demo/mydemo1.c
+++ demo/mydemo1.c
@@ -10,8 +10,12 @@ static inline long csie_ncu_survey_TT(void) { return syscall(__NR__csie_ncu_surv
 
 int main()
 {
-	printf("Hello World!\n");
+	int i,j, array[100][100];                  
+
+	for(i=0;i<100;i++)
+		for(j=0;j<100;j++)
+			array[i][j]=i*j;	
+		
 	csie_ncu_survey_TT();
-	while(1);
-        return 0;
+	return 0;
 }
--- demo/mydemo3.c
+++ demo/mydemo3.c
@@ -1,17 +1,20 @@
-/* https://unix.stackexchange.com/questions/223888/how-to-add-a-system-call-in-linux-kernel-4-x */
 
 #define _GNU_SOURCE         /* See feature_test_macros(7) */
 #include <stdio.h>
 #include <linux/kernel.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <string.h>
 #define __NR__csie_ncu_survey_TT 352
 static inline long csie_ncu_survey_TT(void) { return syscall(__NR__csie_ncu_survey_TT,NULL); }
 
-int main()
-{
-	printf("Hello World!\n");
-	csie_ncu_survey_TT();
-	while(1);
-        return 0;
-}
+
+
+#define GREETING "How are you?\n"
+
+main()
+{ char  greeting_array[1000]; 
+
+	strcpy(greeting_array, GREETING); 
+	csie_ncu_survey_TT(); 
+}  
